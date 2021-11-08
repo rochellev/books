a0f4d8c66ee5e549a6302cd329b39a5c23e3ee3c
rochellev <rochellevroberts@utexas.edu>
2021-11-08 14:55:04 -0800

    successfully displayed Card component in App, marks the begining of dev story

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {Card} from "./components/Card" 
 
 function App() {
   return (
     <div className="App">
       <text>Oh, hello</text>
+      <Card />
     </div>
   );
 }
