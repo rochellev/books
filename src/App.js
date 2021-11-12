bdb923422a548c09372fd9c0a1865e78ac4013a1
rochellev <rochellevroberts@utexas.edu>
2021-11-12 11:51:25 -0800

    added import statement for React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import {Card} from "./components/Card" 
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import { Card } from "./components/Card";
 
 function App() {
   return (
